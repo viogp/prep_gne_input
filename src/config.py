b7057900c6b8ec52d352806b5f6c9a0bc50ccab3
Violeta Gonzalez-Perez <violetagp@protonmail.com>
2025-10-20 18:10:41 +0200

    Adaptation to UNIT_1GP, config generated now testing. Need to output in different path

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 """
 Configuration of files to be read
 """
-sims = ['GP20']
+sims = ['GP20','GP20UNIT1Gpc']
 
 def get_config(simtype, snap, laptop=False, verbose=False):
     """
@@ -32,19 +32,17 @@ def get_config(simtype, snap, laptop=False, verbose=False):
 
     function_name = f'get_{simtype}_config'
     config_function = globals()[function_name]
-    config = config_function(simtype, snap,
+    config = config_function(snap,
                              laptop=laptop, verbose=verbose)
     return config
 
 
-def get_GP20_config(simtype, snap, laptop=False, verbose=False):
+def get_GP20_config(snap, laptop=False, verbose=False):
     """
     Get configuration for GP20 runs
     
     Parameters
     ----------
-    simtype : str
-        Simulation type (should be 'GP20')
     snap : integer
         Snapshot number
     laptop : bool, optional
@@ -123,6 +121,99 @@ def get_GP20_config(simtype, snap, laptop=False, verbose=False):
         'tosedfit.hdf5': {
             'group': 'Output001',
             'datasets': ['mag_UKIRT-K_o_tot_ext', 'mag_SDSSz0.1-r_o_tot_ext'],
+            'units': ['AB', 'AB']
+        }
+    }
+    
+    return config
+
+
+def get_GP20UNIT1Gpc_config(snap, laptop=False, verbose=False):
+    """
+    Get configuration for UNIT 1Gpc runs
+    
+    Parameters
+    ----------
+    snap : integer
+        Snapshot number
+    laptop : bool, optional
+        If True, use local test configuration
+    verbose : bool, optional
+        If True, print further messages
+    
+    Returns
+    -------
+    config: dict
+        Configuration dictionary
+    """
+    # Path to files
+    path = '/data2/users/olivia/galform_output/UNIT_PNG/UNIT_1GPC/'
+    root = path+'iz'+str(snap)+'/ivol'
+    if laptop:
+        root = '/home/violeta/buds/emlines/gp20data/iz'+str(snap)+'/ivol'
+
+    boxside = 30 #Mpc/h (whole volume 1000Mpc/h)
+    
+    config = {
+        # Paths
+        'root': root,
+        
+        # Cosmology parameters
+        'h0': 0.6773999929428101,
+        'omega0': 0.30889999866485596,
+        'omegab': 0.04859999939799309,
+        'lambda0': 0.691100001335144,
+        'boxside': boxside,
+        'mp': 1.24718e9 ,  # Msun/h
+
+        # Metallicity calculation parameters
+        'mcold_disc': 'mcold',
+        'mcold_z_disc': 'cold_metal',
+        'mcold_burst': 'mcold_burst',
+        'mcold_z_burst': 'metals_burst',
+    }
+    config['snap'] = snap
+    
+    # File selection criteria
+    config['selection'] = {
+        'galaxies.hdf5': {
+            'group': 'Output001',
+            'datasets': ['mhhalo', 'xgal', 'ygal', 'zgal'],
+            'units': ['Msun/h', 'Mpc/h', 'Mpc/h', 'Mpc/h'],
+            'low_limits': [20 * config['mp'], 0., 0., 0.],
+            'high_limits': [None, boxside, boxside, boxside]
+        }
+    }
+    
+    # File properties to extract
+    config['file_props'] = {
+        'galaxies.hdf5': {
+            'group': 'Output001',
+            'datasets': ['redshift', 'index', 'type',
+                         'vxgal', 'vygal', 'vzgal',
+                         'rbulge', 'rcomb', 'rdisk', 'mhot', 'vbulge',
+                         'mcold', 'mcold_burst', 'cold_metal', 'metals_burst',
+                         'mstars_bulge', 'mstars_burst', 'mstars_disk',
+                         'mstardot', 'mstardot_burst', 'mstardot_average',
+                         'M_SMBH', 'SMBH_Mdot_hh', 'SMBH_Mdot_stb', 'SMBH_Spin'],
+            'units': ['redshift', 'Host halo index', 'Gal. type (central=0)',
+                      'km/s','km/s','km/s',
+                      'Mpc/h', 'Mpc/h', 'Mpc/h', 'Msun/h', 'km/s',
+                      'Msun/h', 'Msun/h', 'Msun/h', 'Msun/h',
+                      'Msun/h', 'Msun/h', 'Msun/h',
+                      'Msun/h/Gyr', 'Msun/h/Gyr', 'Msun/h/Gyr',
+                      'Msun/h', 'Msun/h/Gyr', 'Msun/h/Gyr', 'Spin']
+        },
+        'agn.hdf5': {
+            'group': 'Output001',
+            'datasets': ['Lbol_AGN'],
+            'units': ['1e40 h^-2 erg/s']
+        },
+        #'tosedfit.hdf5': {
+        'samp_dust.hdf5': {
+            'group': 'Output001',
+            #'datasets': ['mag_UKIRT-K_o_tot_ext', 'mag_SDSSz0.1-r_o_tot_ext'],
+            'datasets': ['mag_WISE-3.6_o_tot_ext', 'mag_SDSS-r_o_tot_ext'],
             'units': ['AB', 'AB']
         }
     }
